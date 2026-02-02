fb1143e7e4e7c14bc0f103c699353f3a893058ab
fly3st <danielhuri.invtb@gmail.com>
2026-02-02 08:46:01 +0200

    backup

---
+++ part2/course-info/src/App.jsx
@@ -0,0 +1,7 @@
+import Course from './components/Course'
+
+const App = () => {
+  return <Course course={course}/>
+}
+
+export default App
---
+++ part2/course-info/src/components/Course.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return <h1>Half Stack application development</h1>
+}
+
+const Content = ({ part }) => {
+
+}
+
+const Part = () => {}
+
+const Course = ({ course }) => {
+}
+
+export default Course
